4b775cde2d2a8a6f9404cb7f184bea490ba265b8
drdivago <matteo.pelliccia@gmail.com>
2021-03-22 19:36:51 +0100

    Add Exercise 39 and 40

---
+++ 1.Fixed-Size Data/3.How to Design Programs/ex40.rkt
@@ -0,0 +1,9 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname ex40) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp")) #f)))
+(define (tock ws)
+  (+ ws 3))
+
+(check-expect (tock 78) 81)
+(check-expect (tock 20) 23)
+(check-expect (tock 25) 29)
